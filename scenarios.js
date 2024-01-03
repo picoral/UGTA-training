95aecdbf771ae3b0a580269c8c269e2b15f85be5
Adriana Picoral <adrianaps@arizona.edu>
2024-01-03 16:14:32 -0700

    initial commit

---
+++ scenarios.js
@@ -0,0 +1,12 @@
+function getOption() {
+            selectElement = document.querySelector('#select1');
+            output = selectElement.value;
+            document.querySelector('.output').textContent = output;
+        }
+        
+        
+function getOptionTwo() {
+            selectElement = document.querySelector('#select2');
+            output = selectElement.value;
+            document.querySelector('.output2').textContent = output;
+        }
